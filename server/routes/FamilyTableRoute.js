0057a1cdcdd960666d76b148c19bba8a2e039072
LeDulan05 <phillipdylanpgarcia@gmail.com>
2025-06-24 17:41:24 +0800

    Added Get and Post

--- server/routes/FamilyTableRoute.js
+++ server/routes/FamilyTableRoute.js
@@ -1,8 +1,19 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
+const {FamilyTable} = require('../models/');
 
-router.get("/", (req,res) =>{
-    res.send("Test")
+
+router.get("/", async(req,res) =>{
+    const familyList = await FamilyTable.findAll();
+    res.json(familyList);
+});
+
+router.post("/", async (req,res) =>{
+    const familyTable = req.body;
+
+    await FamilyTable.create(familyTable);
+    res.json(familyTable);
 })
+
 
 module.exports = router
