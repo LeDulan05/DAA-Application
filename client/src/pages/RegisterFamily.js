15c02263e47366c4b635efcfb2fc236e334fa2ac
LeDulan05 <phillipdylanpgarcia@gmail.com>
2025-06-24 19:04:44 +0800

    Implemented Yup

--- client/src/pages/RegisterFamily.js
+++ client/src/pages/RegisterFamily.js
@@ -1,16 +1,53 @@
 import React from 'react'
 import {Formik, Form, Field, ErrorMessage} from "formik";
+import * as Yup from 'yup'
 
 function RegisterFamily() {
+
+    const intialValues ={
+        Name:"",
+        Address:"",
+        Contact:"",
+    }
+
+    const validationSchema = Yup.object().shape({
+        Name: Yup.string().required(),
+        Address: Yup.string(),
+        Contact: Yup.string()
+    })
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
   return (
     <div className='RegisterFamilyPage'>
-        <Formik >
+        <Formik initialValues={intialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
             <Form>
                 <label>Family Name:</label>
-                <Field 
+                <ErrorMessage name="Name" component="span"/>
+                <Field
+                autocomplete="off" 
                 id="inputRegisterFamily" 
                 name="Name" 
                 placeholder="Garcia"/>
+
+                <label>Address:</label>
+                <ErrorMessage name="Address" component="span"/>
+                <Field 
+                autocomplete="off"
+                id="inputRegisterFamily" 
+                name="Address" 
+                placeholder="7th Street"/>
+
+                <label>Contact:</label>
+                <ErrorMessage name="Address" component="span"/>
+                <Field 
+                autocomplete="off"
+                id="inputRegisterFamily" 
+                name="Contact" 
+                placeholder="0991234141"/>
+
+                <button type = "submit">Register Family</button>
             </Form>
         </Formik>
     </div>
